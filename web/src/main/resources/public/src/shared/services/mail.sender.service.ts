4f8e2dfc0d53dc063c2ba17436e911cbb16304d7
aska <aska.fed@gmail.com>
2016-08-25 17:54:44 +0300

    fix headers in mail service

--- web/src/main/resources/public/src/shared/services/mail.sender.service.ts
+++ web/src/main/resources/public/src/shared/services/mail.sender.service.ts
@@ -12,8 +12,10 @@ import {Mail} from "../models/mail.interface";
 @Injectable()
 export class MailService {
     private url = ApiService.serverUrl + '/restful/mail/';
+    private headers = new Headers({'Authorization': 'Bearer ' + localStorage.getItem('token')});
 
     constructor(private _http:Http){
+        this.headers.append('Content-Type', 'application/json');
     }
 
     sendMail(mail : Mail){
@@ -21,7 +23,7 @@ export class MailService {
         headers.append('Content-Type', 'application/json');
         console.log("sending http POST to " + this.url);
         console.log("json obj: " + JSON.stringify(mail));
-        return this._http.post(this.url, JSON.stringify(mail), {headers: headers})
+        return this._http.post(this.url, JSON.stringify(mail), {headers: this.headers})
             .toPromise()
             .then(()=>mail)
             .catch((err)=>console.error(err));
